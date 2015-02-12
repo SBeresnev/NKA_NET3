980c282aa6a907c46c5424c8393743412b11e873
SBeresnev <ya.beresnev@ya.ru>
2015-02-12 13:02:09 +0300

    Wednesday change

--- services/src/main/java/nla/local/services/impl/DictionaryServiceImp.java
+++ services/src/main/java/nla/local/services/impl/DictionaryServiceImp.java
@@ -1,12 +1,15 @@
 package nla.local.services.impl;
 
 import nla.local.dao.BaseDao;
+import nla.local.dao.exceptions.DaoException;
 import nla.local.pojos.dict.Dict;
 import nla.local.services.IService;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * Created by beresnev on 06.02.2015.
@@ -19,6 +22,13 @@ public class DictionaryServiceImp<T extends Dict> extends BaseDao<T> implements 
     public DictionaryServiceImp(SessionFactory sessionFactory)
     {
         super(sessionFactory);
+
+    }
+
+    public List<T> getAll(Class<T> clazz) throws DaoException {
+
+        return super.getAll(clazz);
+
 
     }
 
