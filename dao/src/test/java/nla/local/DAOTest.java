681b3bb91dae7f819fd77f5243cb29f3def3de29
SBeresnev <ya.beresnev@ya.ru>
2015-02-23 17:17:38 +0300

    Monday  change

--- dao/src/test/java/nla/local/DAOTest.java
+++ dao/src/test/java/nla/local/DAOTest.java
@@ -4,15 +4,11 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:beans-dao.xml"})
-@TransactionConfiguration(defaultRollback = true)
-@Transactional
 public class DAOTest
 {
 
