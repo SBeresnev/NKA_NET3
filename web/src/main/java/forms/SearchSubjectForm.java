876c1bad78bd9e6ba53bf5d6f7f0d71801536ef1
dunkan-zu-zu <vini-vidi-vitchi@yandex.ru>
2015-02-24 13:40:35 +0300

    24.02.2015

--- web/src/main/java/forms/SearchSubjectForm.java
+++ web/src/main/java/forms/SearchSubjectForm.java
@@ -4,16 +4,25 @@ package forms;
  * Created by belonovich on 06.02.2015.
  */
 public class SearchSubjectForm {
-private String name;
+    private String name;
     private Integer type;
     private String number;
+
+    public String surname = "";
+    public String firstname = "";
+    public String lastname = "";
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name)
-    {
+    public void setName(String name) {
+            String[] names = name.split(" ");
+            try{
+                this.firstname = names[0];
+                this.surname = names[1];
+                this.lastname = names[2];
+            } catch (Exception e){}
             this.name = name;
     }
 
