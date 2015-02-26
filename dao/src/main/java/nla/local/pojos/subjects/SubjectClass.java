96a5969add82365577f64603a7ff1aead34de3cf
SBeresnev <ya.beresnev@ya.ru>
2015-02-26 09:31:24 +0300

    Thursday  change

--- dao/src/main/java/nla/local/pojos/subjects/SubjectClass.java
+++ dao/src/main/java/nla/local/pojos/subjects/SubjectClass.java
@@ -21,5 +21,4 @@ public enum SubjectClass {
         return value;
     }
 
-
 };
---
+++ services/src/main/java/nla/local/services/impl/subjects/PassportServiceImp.java
@@ -0,0 +1,32 @@
+package nla.local.services.impl.subjects;
+
+import nla.local.pojos.subjects.PassportNCA;
+import nla.local.pojos.subjects.RespNCA;
+import nla.local.services.IPassportService;
+import nla.local.services.MVDutil.PassportNCAResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ws.client.core.WebServiceTemplate;
+
+import javax.xml.bind.JAXBElement;
+
+/**
+ * Created by beresnev on 25.02.2015.
+ */
+@Service
+public class PassportServiceImp implements IPassportService{
+
+    @Autowired
+    WebServiceTemplate webServiceTemplate;
+
+    @Override
+    public RespNCA findSubject(PassportNCA PassNCA) {
+
+        JAXBElement<PassportNCAResponse> jaxrespNCA = (JAXBElement<PassportNCAResponse>) webServiceTemplate.marshalSendAndReceive(PassNCA);
+
+        RespNCA ret_val = jaxrespNCA.getValue().getReturn();
+
+        return ret_val;
+    }
+
+}
