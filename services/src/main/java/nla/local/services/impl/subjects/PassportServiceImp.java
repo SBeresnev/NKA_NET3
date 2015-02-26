5fd3f5a1bc4f306924daf49119d67cff1bb4b6ed
SBeresnev <ya.beresnev@ya.ru>
2015-02-26 09:35:29 +0300

    Thursday  change

--- services/src/main/java/nla/local/services/impl/subjects/PassportServiceImp.java
+++ services/src/main/java/nla/local/services/impl/subjects/PassportServiceImp.java
@@ -4,7 +4,9 @@ import nla.local.pojos.subjects.PassportNCA;
 import nla.local.pojos.subjects.RespNCA;
 import nla.local.services.IPassportService;
 import nla.local.services.MVDutil.PassportNCAResponse;
+import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.springframework.ws.client.core.WebServiceTemplate;
 
@@ -14,10 +16,14 @@ import javax.xml.bind.JAXBElement;
  * Created by beresnev on 25.02.2015.
  */
 @Service
-public class PassportServiceImp implements IPassportService{
+public class PassportServiceImp extends PSubjectServiceImp implements IPassportService{
 
     @Autowired
     WebServiceTemplate webServiceTemplate;
+
+    public PassportServiceImp(@Qualifier("sessionFactory") SessionFactory sessionFactory) {
+        super(sessionFactory);
+    }
 
     @Override
     public RespNCA findSubject(PassportNCA PassNCA) {
