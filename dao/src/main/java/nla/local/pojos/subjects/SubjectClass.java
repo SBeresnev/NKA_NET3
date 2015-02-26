e501c17ba5f80caae46644eb4e02be3f763a6090
SBeresnev <ya.beresnev@ya.ru>
2015-02-26 09:47:23 +0300

    Thursday  change

--- dao/src/main/java/nla/local/pojos/subjects/SubjectClass.java
+++ dao/src/main/java/nla/local/pojos/subjects/SubjectClass.java
@@ -1,14 +1,18 @@
 package nla.local.pojos.subjects;
 
 /**
+ * содержит справочную инфлормацию о типах субъектов. Числа соответствуют полю PARENT_CODE
+ * из таблицы ANALYTICCODES, ANALYTIC_TYPE = 110
  * Created by Serega on 22.02.2015.
  */
+
 
 
 public enum SubjectClass {
     PRV("private"),
     JUR("juridical"),
-    OFC("official");
+    OFC("official"),
+    UKW("unknown");
 
     private String value;
 
@@ -21,4 +25,36 @@ public enum SubjectClass {
         return value;
     }
 
+
+    public int toInt(SubjectClass value) {
+
+        switch(value) {
+
+            case JUR: return 200;
+
+            case PRV: return 100;
+
+            case OFC: return 600;
+
+            default:
+
+                return -1;
+        }
+
+    }
+
+    public static SubjectClass fromInt(int value) {
+        switch(value) {
+
+            case 200: return JUR;
+
+            case 100: return PRV;
+
+            case 600: return OFC;
+
+            default:
+
+                return UKW;
+        }
+    }
 };
