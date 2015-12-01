7ece54c239eabfdd73766cc7b80598fe4948fb8d
SBeresnev <ya.beresnev@ya.ru>
2015-12-01 09:34:56 +0300

    Monday change  (30.11.2015)

--- web/src/main/java/nla/local/exception/SubjectControllerException.java
+++ web/src/main/java/nla/local/exception/SubjectControllerException.java
@@ -1,5 +1,7 @@
 package nla.local.exception;
 
+import java.nio.charset.Charset;
+
 /**
  * Created by beresnev on 24.11.2015.
  */
@@ -11,5 +13,9 @@ public class SubjectControllerException extends RuntimeException {
     public SubjectControllerException(String villageName) {
 
         super(villageName);
+
+        String value = new String(villageName.getBytes(Charset.forName("UTF-8")));
+
+
     }
 }
